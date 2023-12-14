83389ede2828b768e064ccc4460fcd591dab1ff1
Sheraz Safdar <sherazsafdar00@gmail.com>
2023-12-14 12:03:52 +0500

    create simple api views

--- ads/urls.py
+++ ads/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 from .views import (
-    AdListView, AdDetailView,
-    LocationListView, LocationDetailView,
-    VisitCountListView, VisitCountDetailView
+    AdCreateView, AdDetailView,
+    #LocationListView, LocationDetailView,
+    #VisitCountListView, VisitCountDetailView
 )
 
 urlpatterns = [
-    path('ads/', AdListView.as_view(), name='ad-list'),
+    path('ads/', AdCreateView.as_view(), name='ad-list'),
     path('ads/<int:pk>/', AdDetailView.as_view(), name='ad-detail'),
-    path('location/', LocationListView.as_view(), name='location-list'),
-    path('location/<int:pk>/', LocationDetailView.as_view(), name='location-detail'),
-    path('visitcounts/', VisitCountListView.as_view(), name='visitcount-list'),
-    path('visitcount/<int:pk>/', VisitCountDetailView.as_view(), name='visitcount-detail'),
+    #path('location/', LocationListView.as_view(), name='location-list'),
+    #path('location/<int:pk>/', LocationDetailView.as_view(), name='location-detail'),
+    #path('visitcounts/', VisitCountListView.as_view(), name='visitcount-list'),
+    #path('visitcount/<int:pk>/', VisitCountDetailView.as_view(), name='visitcount-detail'),
 ]
 
--- ads/views.py
+++ ads/views.py
@@ -1,46 +1,67 @@
-from rest_framework import viewsets
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import BasicAuthentication
 from .models import Location,Ad,VisitCount
 from .serializers import AdSerializer, LocationSerializer, VisitCountSerializer
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
 
-class AdListView(generics.ListCreateAPIView):
-    queryset = Ad.objects.all()
-    serializer_class = AdSerializer
+
+
+class AdCreateView(APIView):
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
     
-    
-class AdDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Ad.objects.all()
-    serializer_class = AdSerializer
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
-    
-class LocationListView(generics.ListCreateAPIView):
-    queryset = Location.objects.all()
-    serializer_class = LocationSerializer
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
-    
-class LocationDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Location.objects.all()
-    serializer_class = LocationSerializer 
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
-    
-class VisitCountListView(generics.ListCreateAPIView):
-    queryset = VisitCount.objects.all()
-    serializer_class = VisitCountSerializer
+    def post(self, request):
+        serializer = AdSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+class AdDetailView(APIView):
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
 
-class VisitCountDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = VisitCount.objects.all()
-    serializer_class = VisitCountSerializer
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    def get_object(self, pk):
+        try:
+            return Ad.objects.get(pk=pk)
+        except Ad.DoesNotExist:
+            return None
+
+    def get(self, pk):
+        ad = self.get_object(pk)
+        if ad is not None:
+            serializer = AdSerializer(ad)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({'error': 'Ad not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, pk):
+        ad = self.get_object(pk)
+        if pk is not None:
+            serializer = AdSerializer(ad, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, stauts=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'error': 'Ad not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+    def patch(self, request, pk):
+        ad = self.get_object(pk)
+        if ad is not None:
+            serializer = AdSerializer(ad, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'error': 'Ad not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self,  pk):
+        ad = self.get_object(pk)
+        if ad is not None:
+            ad.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({'error': 'Ad not found.'}, status=status.HTTP_404_NOT_FOUND)     
     
-    
-#    
